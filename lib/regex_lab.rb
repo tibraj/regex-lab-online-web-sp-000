c9578a0915dbbef4c9f3a271b7e4dd5628808240
Taulant Ibraj <tibraj94@gmail.com>
2020-03-26 15:56:27 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,10 +1,10 @@
 def starts_with_a_vowel?(word)
-if word.match(/[^a|e|i|o|u]/)
-return true
-else
-  return false
-end
-end
+ if word.match(/^[aeiouAEIOU]\w+/)
+   return true
+ else
+    false
+  end
+  end
 
 def words_starting_with_un_and_ending_with_ing(text)
 
